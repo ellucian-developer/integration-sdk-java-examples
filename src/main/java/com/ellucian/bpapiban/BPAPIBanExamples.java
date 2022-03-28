e740bf2ca3c50200a28379316132e82328dcfc57
dfkumar <david.kumar@sbcglobal.net>
2022-03-28 11:07:17 -0500

    Added JsonNode person-comments POST/PUT example.

--- src/main/java/com/ellucian/bpapiban/BPAPIBanExamples.java
+++ src/main/java/com/ellucian/bpapiban/BPAPIBanExamples.java
@@ -13,6 +13,8 @@ import com.ellucian.ethos.integration.client.proxy.filter.FilterMap;
 import com.ellucian.generated.bpapi.ban.person_comments.v1_0_0.PersonComments100PostRequest;
 import com.ellucian.generated.bpapi.ban.person_search.v1_0_0.PersonSearch100GetResponse;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -31,9 +33,22 @@ public class BPAPIBanExamples extends ExamplesBase {
     public void runExamples() {
         System.out.println( "Running BPAPI SDK examples..." );
         applyPersonCommentsUsingJavaBeans();
+        applyPersonCommentsUsingJsonNodes();
     }
 
+    /**
+     * This is an example of using the Ethos Integration SDK with the Ethos Integration SDK objects library.
+     * This library contains strongly typed objects (JavaBeans).  JavaBeans are like data transfer objects (DTOs)
+     * that help manage the requests and responses from API calls through the SDK.
+     * This example does the following:
+     * 1) Finds a person ID using the person-search API with a lastName filter.
+     * 2) Builds a PersonComments100PostRequest object with request body data.
+     * 3) Makes a POST request to the person-comments API to apply a new person comment for the given person ID.
+     * 4) Changes the contactDate field for the person comment.
+     * 5) Makes a PUT request to update the person comment for the given person ID.
+     */
     public void applyPersonCommentsUsingJavaBeans() {
+        // Use an EthosFilterQueryClient since we need to make a filter GET request.  Can also use it to make POST and PUT requests since it extends EthosProxyClient.
         EthosFilterQueryClient ethosFilterQueryClient = getEthosFilterQueryClient();
         try {
             System.out.println("\n\n--- Running apply person comments JavaBeans example... ---");
@@ -64,29 +79,42 @@ public class BPAPIBanExamples extends ExamplesBase {
                                                                 .withId(personId)
                                                                 .withConfidentialInd("s")
                                                                 .withOrigCode("CCON")
-                                                                .withText("testing through Java SDK")
-                                                                .withTextNar("more testing through Java SDK")
+                                                                .withText("adding a comment through Java SDK")
+                                                                .withTextNar("comment through Java SDK")
                                                                 .withContactDate(today)
                                                                 .withDate(today);
-
+            resourceName = "person-comments";
             // Make a POST request to add a new person comment using a JavaBean request body object.
             System.out.println( "Making POST request to add a new person comment for person with ID: " + personId + ", using contact date: " + personCommentRequest.getContactDate() );
             ethosResponse = ethosFilterQueryClient.post( resourceName, personCommentRequest );
+            System.out.println( "POST made using this URL: " + ethosResponse.getRequestedUrl() );
             System.out.println( "POST EthosResponse body: " + ethosResponse.getContent() );
 
             // Change the contact date on the personCommentRequest so that we can make a PUT request to update it.  Adds a day to the date.
             personCommentRequest.setContactDate( Date.from(Instant.now().plus(1, ChronoUnit.DAYS)) );
             System.out.println( "Making PUT request with updated contact date: " + personCommentRequest.getContactDate() );
             ethosResponse = ethosFilterQueryClient.put( resourceName, personCommentRequest );
+            System.out.println( "PUT made using this URL: " + ethosResponse.getRequestedUrl() );
             System.out.println( "PUT EthosResponse body: " + ethosResponse.getContent() );
-
         }
         catch( Exception e ) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * This is an example of using the Ethos Integration SDK with the Jackson JsonNode library.
+     * Jackson is a popular Java library for handling JSON formatted data, which can be used with the Ethos Integration SDK
+     * to help manage the requests and responses from API calls through the SDK.
+     * This example does the following:
+     * 1) Finds a person ID using the person-search API with a lastName filter.
+     * 2) Builds a POST request body as a JsonNode object to add a person comment using the person-comment API for given person ID.
+     * 3) Makes a POST request to the person-comments API to apply a new person comment for the given person ID.
+     * 4) Changes the contactDate field for the person comment.
+     * 5) Makes a PUT request using a JsonNode request body to update the person comment for the given person ID.
+     */
     public void applyPersonCommentsUsingJsonNodes() {
+        // Use an EthosFilterQueryClient since we need to make a filter GET request.  Can also use it to make POST and PUT requests since it extends EthosProxyClient.
         EthosFilterQueryClient ethosFilterQueryClient = getEthosFilterQueryClient();
         try {
             System.out.println("\n\n--- Running apply person comments JsonNodes example... ---");
@@ -106,40 +134,40 @@ public class BPAPIBanExamples extends ExamplesBase {
             String personId = personSearchNode.elements().next().get("id").asText();
             System.out.println( "Found person with ID: " + personId );
 
-            // Now build a person comment using JsonNode with this structure.
+//             Now build a person comment using JsonNode with this structure.
 //            {
 //                "date": "10/02/2022T20:53:54.872Z",
-//                    "contactDate": "10/02/2022",
-//                    "cmttCode": "105",
-//                    "id": "210009506",
-//                    "confidentialInd": "s",
-//                    "text": "testing, testing, 1234",
-//                    "origCode": "CCON",
-//                    "textNar": "1234, testing, testing"
+//                "contactDate": "10/02/2022",
+//                "cmttCode": "105",
+//                "id": "210009506",
+//                "confidentialInd": "s",
+//                "text": "testing, testing, 1234",
+//                "origCode": "CCON",
+//                "textNar": "1234, testing, testing"
 //            }
 
-
-            Instant now = Instant.now();
-            Date today = Date.from( now );
-            PersonComments100PostRequest personCommentRequest = new PersonComments100PostRequest()
-                    .withCmttCode("105")
-                    .withId(personId)
-                    .withConfidentialInd("s")
-                    .withOrigCode("CCON")
-                    .withText("testing through Java SDK")
-                    .withTextNar("more testing through Java SDK")
-                    .withContactDate(today)
-                    .withDate(today);
+            Instant rightNow = Instant.now().truncatedTo( ChronoUnit.SECONDS );
+            // Use a JsonNode for the POST request body.
+            ObjectNode personCommentNode = JsonNodeFactory.instance.objectNode();
+            personCommentNode.put( "id", personId );
+            personCommentNode.put( "date", rightNow.toString() );
+            personCommentNode.put( "contactDate", rightNow.toString() );
+            personCommentNode.put( "cmttCode", "105" );
+            personCommentNode.put( "confidentialInd", "s" );
+            personCommentNode.put( "text", "Testing through BPAPI Java SDK example" );
+            personCommentNode.put( "origCode", "CCON" );
+            personCommentNode.put( "textNar", "Testing through BPAPI Java SDK example" );
 
             // Make a POST request to add a new person comment using a JavaBean request body object.
-            System.out.println( "Making POST request to add a new person comment for person with ID: " + personId + ", using contact date: " + personCommentRequest.getContactDate() );
-            ethosResponse = ethosFilterQueryClient.post( resourceName, personCommentRequest );
+            System.out.println( "Making POST request to add a new person comment for person with ID: " + personId + ", using contact date: " + personCommentNode.get("contactDate") );
+            resourceName = "person-comments";
+            ethosResponse = ethosFilterQueryClient.post( resourceName, personCommentNode );
             System.out.println( "POST EthosResponse body: " + ethosResponse.getContent() );
 
-            // Change the contact date on the personCommentRequest so that we can make a PUT request to update it.  Adds a day to the date.
-            personCommentRequest.setContactDate( Date.from(Instant.now().plus(1, ChronoUnit.DAYS)) );
-            System.out.println( "Making PUT request with updated contact date: " + personCommentRequest.getContactDate() );
-            ethosResponse = ethosFilterQueryClient.put( resourceName, personCommentRequest );
+            // Change the contact date on the personCommentNode so that we can make a PUT request to update it.  Adds a day to the date.
+            personCommentNode.put( "contactDate", rightNow.plus(1, ChronoUnit.DAYS).toString() );
+            System.out.println( "Making PUT request with updated contact date: " + personCommentNode.get("contactDate") );
+            ethosResponse = ethosFilterQueryClient.put( resourceName, personCommentNode );
             System.out.println( "PUT EthosResponse body: " + ethosResponse.getContent() );
 
         }
